4b963283e1b9d5450c8a55fcdef81b057adb0a92
NewAccdlyalb <74828202+NewAccdlyalb@users.noreply.github.com>
2020-11-23 20:38:16 +0300

    Add files via upload

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b;
+            int menu, menu1 = 1;
+
+            while (menu1 == 1) { 
+                Console.WriteLine("Введите первое число: ");
+                a = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Введите второе число: ");
+                b = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Что вы хотите сделать с числами?");
+                Console.WriteLine("1 - Сложить");
+                Console.WriteLine("2 - Вычесть");
+                Console.WriteLine("3 - Умножить");
+                Console.WriteLine("4 - Разделить");
+
+
+                menu = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Ответ: ");
+                if (menu == 1) Console.WriteLine(a + b);
+                if (menu == 2) Console.WriteLine(a - b);
+                if (menu == 3) Console.WriteLine(a * b);
+                if (menu == 4)
+                {
+                    if (b == 0) Console.WriteLine("На ноль делить нельзя!");
+                    else
+                    {
+                        Console.WriteLine(a / b);
+                    }
+                }
+                
+                
+
+                Console.WriteLine(" ");
+            }
+
+        }
+    }
+}
